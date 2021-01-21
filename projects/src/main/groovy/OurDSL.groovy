b12b5d153d08bc579d0449583ade8dd856914a14
ihab abadi <ihab@utopios.net>
2021-01-21 17:11:38 +0100

    correction dsl

--- projects/src/main/groovy/OurDSL.groovy
+++ projects/src/main/groovy/OurDSL.groovy
@@ -1,4 +1,7 @@
+import groovy.transform.ToString
+
 //DSL
+/*
 class Cookie {
     List ingredient
 
@@ -11,7 +14,7 @@ class Cookie {
         closure()
     }
 
-    def ingredient(String element) {
+    def ingredient( element) {
         ingredient << element
     }
 }
@@ -23,4 +26,57 @@ c.make {
     ingredient "farine"
 }
 
-println(c.ingredient)
+println(c.ingredient)*/
+@ToString
+class Email {
+    String from
+    String to
+    List cc
+    String subject
+    String body
+    List attach
+
+    Email() {
+        cc = new ArrayList()
+        attach = new ArrayList()
+    }
+
+    def send(def cl) {
+        cl.delegate = this
+        cl()
+    }
+
+    def from(String from) {
+        this.from = from
+    }
+    def to(String to) {
+        this.to = to
+    }
+    def cc(String cc) {
+        this.cc << cc
+    }
+
+    def subject(String subject) {
+        this.subject = subject
+    }
+
+    def body(String body) {
+        this.body = body
+    }
+
+    def attach(String attach) {
+        this.attach << attach
+    }
+}
+
+Email e = new Email()
+e.send {
+    from "ihab@utopios.net"
+    to "toto@tata.fr"
+    cc "titi@minet.fr"
+    subject "test"
+    body : "body mail"
+    attach : "file1"
+}
+
+println(e)
