8ca84dc6515483069200738141797b68a241b26a
ihab abadi <ihab@utopios.net>
2021-02-05 16:44:24 +0100

    exemple tdd reviewService

--- DemoEcommerce/grails-app/services/demoecommerce/ReviewService.groovy
+++ DemoEcommerce/grails-app/services/demoecommerce/ReviewService.groovy
@@ -6,8 +6,14 @@ import grails.transaction.Transactional
 @Transactional
 class ReviewService {
     ReviewRepository reviewRepository
-
+    ProductService productService
     List<Review> getReviewsByProductId(int id) {
+        //test 1-> Product Not Exist
+        //test 2-> Product Exist
+        throw new Exception()
+    }
+
+    boolean saveReview(Review review) {
         throw new Exception()
     }
 }
--- DemoEcommerce/test/unit/demoecommerce/ReviewServiceSpec.groovy
+++ DemoEcommerce/test/unit/demoecommerce/ReviewServiceSpec.groovy
@@ -1,5 +1,7 @@
 package demoecommerce
 
+import formation.repositories.ReviewRepository
+import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
@@ -7,14 +9,37 @@ import spock.lang.Specification
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
  */
 @TestFor(ReviewService)
+@Mock([Product, Review])
 class ReviewServiceSpec extends Specification {
-
+    def reviewReposMock
+    def productServiceMock
     def setup() {
+        reviewReposMock = mockFor(ReviewRepository)
+        productServiceMock = mockFor(ProductService)
     }
 
     def cleanup() {
     }
 
-    void "test something"() {
+    void "Product Not Exist"() {
+        given:
+        productServiceMock.demand.findProduct{int id-> throw new Exception("Product not found")}
+        service.productService = productServiceMock.createMock()
+        when:
+        def result = service.getReviewsByProductId(1)
+        then:
+        Exception e = thrown()
+        e.message == "Product not exist"
+    }
+
+    void "Reviews when Product Exist"() {
+        given:
+        Product p = new Product(title: 'test', id: 1)
+        productServiceMock.demand.findProduct{int id-> p}
+        reviewReposMock.demand.getReviewByProductId {id -> [new Review(content: 'content 1', product: p)]}
+        when:
+        def result = service.getReviewsByProductId(1)
+        then:
+        result.size() == 1
     }
 }
